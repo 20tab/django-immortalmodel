43ff306466a8a05161ec13796b008cfc5034f0b2
Raffaele Colace <raffaele@20tab.com>
2019-04-20 22:47:17 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 import immortalmodel
 setup(
-    name='twentytab-immortalmodel',
+    name='django-immortalmodel',
     version=immortalmodel.__version__,
     author='20Tab S.r.l.',
     author_email='info@20tab.com',
     description='A django model, manager and queryset implementing undeletable models',
-    url='https://github.com/20tab/twentytab-immortalmodel',
+    url='https://github.com/20tab/django-immortalmodel',
     packages=['immortalmodel'],
     license='MIT License',
     install_requires=[
-        'Django >=1.6',
+        'Django >=2.1',
     ],
 )
